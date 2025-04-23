747729f6400591c0578ceb1766cc02fd344a1f69
Jorge Patino <Jmp17450@uga.edu>
2025-04-23 19:14:44 -0400

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,4 @@
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import EmailIcon from '@mui/icons-material/Email';
+
 
 function Contact() {
 
