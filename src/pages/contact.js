3f434778eaded891995132a402a8155fe52ea9c4
Jorge Patino <Jmp17450@uga.edu>
2025-05-18 17:07:05 -0400

    contact added

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,8 @@
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import './contact.css';
+import * as React from 'react';
+import Link from '@mui/material/Link'
 
 function Contact() {
 
@@ -10,18 +11,17 @@ function Contact() {
             <h1>Contact Me!</h1>
             <p>This is the contact page. Plus other of my pages</p>
             <div className='each-one'>
-                <div className='phone'> 
-                    <PhoneIphoneIcon />
-                    <a href="tel:+1234567890">+1 (234) 567-890</a>
-
+                <div className='each'> 
+                    <LinkedInIcon sx={{ fontSize: 64 }} />
+                    <p>
+                        Lets connect! here is my <Link color="inherit" href="https://www.linkedin.com/in/jpatino3014/" target="_blank" rel="noopener noreferrer">LinkedIn</Link>
+                    </p>
                 </div>
-                <div className='linkedin'> 
-                    <LinkedInIcon />
-                    <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                </div>
-                <div className='github'>
-                    <GitHubIcon />
-                    <a href="www.github.com/yourprofile" target="_blank" rel="noopener noreferrer">GitHub</a>
+                <div className='each'>
+                    <GitHubIcon sx={{ fontSize: 64 }} />
+                    <p>
+                        Check out my <Link color="inherit"  href="https://github.com/jorgeP301" target="_blank" rel="noopener noreferrer">GitHub</Link>
+                    </p>
                 </div>
             </div>
         </div>
