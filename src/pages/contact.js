96ae46f6213a49a0597d22ba3d3c0860c64edb0e
Jorge Patino <Jmp17450@uga.edu>
2025-05-18 14:52:57 -0400

    dependency issue fixed

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,29 @@
-
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import './contact.css';
 
 function Contact() {
 
     return (
-        <div className="container">
+        <div className="contact">
             <h1>Contact Me!</h1>
             <p>This is the contact page. Plus other of my pages</p>
+            <div className='each-one'>
+                <div className='phone'> 
+                    <PhoneIphoneIcon />
+                    <a href="tel:+1234567890">+1 (234) 567-890</a>
+
+                </div>
+                <div className='linkedin'> 
+                    <LinkedInIcon />
+                    <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                </div>
+                <div className='github'>
+                    <GitHubIcon />
+                    <a href="www.github.com/yourprofile" target="_blank" rel="noopener noreferrer">GitHub</a>
+                </div>
+            </div>
         </div>
     );
 }
